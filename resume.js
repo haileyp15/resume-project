55119b4c3e8ead8a78327526dd575211a59ce006
haileyp15 <parkerhn@oru.edu>
2025-09-17 09:19:01 -0500

    Create resume.js

---
+++ resume.js
@@ -0,0 +1,13 @@
+// Define Variables
+let myName= "Hailey";
+let title= "Student";
+let experience= "Barista, Office Receptionist, & Camp Counselor;
+let education= "Oral Robert's University";
+let skills= ["Dancing", "Coffee Making", "Leadership"];
+
+//Connect Variables to the html
+document.getElementById("my-name").textContent = myName;
+document.getElementById("title").textContent = title;
+document.getElementById("experience").textContent = experience;
+document.getElementById("education").textContent = education;
+
